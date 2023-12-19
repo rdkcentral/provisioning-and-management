39b2cd109e1a2d5d3a5a7f771af0048b4bb21120
Andre McCurdy <armccurdy@gmail.com>
2023-12-18 17:50:07 -0800

    RDKCOM-5442: RDKBDEV-3232 drop obsolete code conditional on ARRIS_XB3_PLATFORM_CHANGES   for file in $(find . -type f -name '*.[ch]') ; do unifdef -B -UARRIS_XB3_PLATFORM_CHANGES -o $file $file ; done Signed-off-by: Andre McCurdy <amccurdy@libertyglobal.com>

--- source-arm/TR-181/board_sbapi/cosa_ethernet_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_ethernet_apis.c
@@ -111,10 +111,11 @@
 #include <utctx/utctx_api.h>
 #include "linux/sockios.h"
 #include <sys/ioctl.h>
+
 #ifdef ARRIS_XB3_PLATFORM_CHANGES
-  #include "rdk_cm_api_arris.h"
+#include "rdk_cm_api_arris.h"
 #else
-  #include "linux/if.h"
+#include <net/if.h>
 #endif
 
 #ifdef CORE_NET_LIB
@@ -337,11 +338,7 @@ CosaDmlEthInit
      *  It doesn't make sense to even have a MAC address in Ethernet Interface DM object,
      *  so we are not going to fill the MAC address for Upstream interfaces.
      */
-#ifdef ARRIS_XB3_PLATFORM_CHANGES
-    rdkb_api_platform_hal_GetLanMacAddr(strMac);
-#else
     _getMac("brlan0", strMac);
-#endif
 
     /*  Iterate through Ethernet ports, assign LAN mac to downstream ports
         Keep track of the index of upstream ports to assign their MAC addresses
