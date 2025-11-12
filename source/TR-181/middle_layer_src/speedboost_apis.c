d137482672da21a21c16201453d1c0cbadb4e9ce
Amaresh-Kotekal <Amaresh_Kotekal@comcast.com>
2025-11-12 00:40:14 +0000

    RDKB-62186:Intermittent low speed below 270 seen with devices configured for 500mbps | XM speedboost build is the first
    Reason for change:Appending the local reserved port with speedboost port range
    Test Procedure: Test speed test functionality
    Risks: high
    Priority: P0

--- source/TR-181/middle_layer_src/speedboost_apis.c
+++ source/TR-181/middle_layer_src/speedboost_apis.c
@@ -48,36 +48,141 @@ static int writeSysctl(const char *pBuf)
     return 0;
 }
 
-void appendToLocalReservedPorts(char * pStartingPort, char * pEndingPort)
+
+void appendToLocalReservedPorts(const char * pPrevVal, char * pCurrentStartPort, char * pCurrentEndPort)
 {
     char cLocalReservedPorts[MAX_VAL_LEN] = {0};
+    char cTmp[MAX_VAL_LEN] = {0};
+    char cOut[MAX_VAL_LEN] = {0};
+    char cPrevRange[64] = {0};
+    char cCurrntRange[64] = {0};
+    char *pToken = NULL, *pSave = NULL;
 
-    if (NULL == pStartingPort || NULL == pEndingPort)
-    {
+    /* validate current range */
+    if (NULL == pCurrentStartPort || NULL == pCurrentEndPort) {
         CcspTraceError(("%s:%d, NULL parameter passed\n", __FUNCTION__, __LINE__));
         return;
     }
-    if (0 == strlen(pStartingPort) || 0 == strlen(pEndingPort))
-    {
+    if (pCurrentStartPort[0] == '\0' || pCurrentEndPort[0] == '\0') {
         CcspTraceError(("%s:%d, Empty port range passed\n", __FUNCTION__, __LINE__));
         return;
     }
-    if (0 != readSysctl(cLocalReservedPorts, sizeof(cLocalReservedPorts)))
-    {
+
+    /* read existing sysctl */
+    if (0 != readSysctl(cLocalReservedPorts, sizeof(cLocalReservedPorts))) {
         CcspTraceError(("%s:%d, Failed to read %s\n", __FUNCTION__, __LINE__, SYSCTL_PATH));
         return;
     }
-    CcspTraceInfo(("%s:%d, Current local reserved ports: %s\n", __FUNCTION__, __LINE__, cLocalReservedPorts));
-    if (0 != strlen(cLocalReservedPorts))
-    {
-        strncat(cLocalReservedPorts, ",", sizeof(cLocalReservedPorts) - strlen(cLocalReservedPorts) - 1);
+
+    CcspTraceInfo(("%s:%d, previous port range: %s\n", __FUNCTION__, __LINE__, pPrevVal));
+    CcspTraceInfo(("%s:%d, current local reserved ports: %s\n", __FUNCTION__, __LINE__, cLocalReservedPorts));
+    /* prepare current range */
+    snprintf(cCurrntRange, sizeof(cCurrntRange), "%s-%s", pCurrentStartPort, pCurrentEndPort);
+
+    /* extract IPv4 start/end from pPrevVal (format: "IPv4 PROTO START END,IPv6 ...") */
+    if (pPrevVal && pPrevVal[0]) {
+        char *pVal = strdup(pPrevVal);
+        if (pVal) {
+            char *pToken1 = NULL, *pSave1 = NULL;
+            for (pToken1 = strtok_r(pVal, ",", &pSave1); pToken1 != NULL; pToken1 = strtok_r(NULL, ",", &pSave1)) {
+                char *pToken = pToken1;
+                while (*pToken == ' ' || *pToken == '\t') pToken++;
+                char *pToken2 = NULL, *pSave2 = NULL;
+                pToken2 = strtok_r(pToken, " ", &pSave2); /* "IPv4" or "IPv6" */
+                if (pToken2 && strcasecmp(pToken2, "IPv4") == 0) {
+                    /* skip proto */
+                    (void)strtok_r(NULL, " ", &pSave2);
+                    char *pStart = strtok_r(NULL, " ", &pSave2);
+                    char *pEnd = strtok_r(NULL, " ", &pSave2);
+                    if (pStart && pEnd) {
+                        /* if prev range span > MAX_PORT_RANGE_DIFF, clamp prev end */
+                        int iPrevStart = atoi(pStart);
+                        int iPrevEnd = atoi(pEnd);
+                        if (iPrevEnd - iPrevStart > MAX_PORT_RANGE_DIFF) {
+                            iPrevEnd = iPrevStart + MAX_PORT_RANGE_DIFF;
+                        }
+                        snprintf(cPrevRange, sizeof(cPrevRange), "%d-%d", iPrevStart, iPrevEnd);
+                        CcspTraceInfo(("%s:%d, extracted previous range: %s\n", __FUNCTION__, __LINE__, cPrevRange));
+                    }
+                    break;
+                }
+            }
+            free(pVal);
+        }
     }
-    strncat(cLocalReservedPorts, pStartingPort, sizeof(cLocalReservedPorts) - strlen(cLocalReservedPorts) - 1);
-    strncat(cLocalReservedPorts, "-", sizeof(cLocalReservedPorts) - strlen(cLocalReservedPorts) - 1);
-    strncat(cLocalReservedPorts, pEndingPort, sizeof(cLocalReservedPorts) - strlen(cLocalReservedPorts) - 1);
-    CcspTraceInfo(("%s:%d, Local reserved ports updated: %s\n", __FUNCTION__, __LINE__, cLocalReservedPorts));
-    if (0 != writeSysctl(cLocalReservedPorts))
-    {
+
+    /* If prev and current are identical, nothing to do */
+    if (cPrevRange[0] != '\0' && strcmp(cPrevRange, cCurrntRange) == 0) {
+        CcspTraceInfo(("%s:%d, Prev range equals current range (%s) - no change\n", __FUNCTION__, __LINE__, cCurrntRange));
+        return;
+    }
+
+    /* Flags to control behavior */
+    bool bCurrentFound = false;
+
+    /* If existing list non-empty, scan tokens.
+       If prev_range present -> rebuild cOut skipping it.
+       If prev_range not present -> keep full list in cOut. */
+    if (cLocalReservedPorts[0] != '\0') {
+        strncpy(cTmp, cLocalReservedPorts, sizeof(cTmp) - 1);
+        for (pToken = strtok_r(cTmp, ",", &pSave); pToken != NULL; pToken = strtok_r(NULL, ",", &pSave)) {
+            /* trim leading/trailing spaces */
+            char *pStr = pToken;
+            while (*pStr == ' ' || *pStr == '\t') pStr++;
+            size_t iLen = strlen(pStr);
+            while (iLen > 0 && (pStr[iLen - 1] == ' ' || pStr[iLen - 1] == '\t' || pStr[iLen - 1] == '\n' || pStr[iLen - 1] == '\r')) {
+                pStr[--iLen] = '\0';
+            }
+
+            if (cPrevRange[0] != '\0' && strcmp(pStr, cPrevRange) == 0) {
+                /* skip adding prev token */
+                continue;
+            }
+
+            if (strcmp(pStr, cCurrntRange) == 0) {
+                bCurrentFound = true;
+            }
+
+            if (cOut[0] != '\0') {
+                strncat(cOut, ",", sizeof(cOut) - strlen(cOut) - 1);
+            }
+            strncat(cOut, pStr, sizeof(cOut) - strlen(cOut) - 1);
+        }
+    }
+
+    /*
+     Behavior required:
+      - if prev_range found and prev != current: remove prev and append current (unless current already present)
+      - if prev_range found and prev == current: do nothing (handled above)
+      - if prev_range NOT found: append current (avoid duplicate)
+    */
+
+    /* If prev was not found we kept original list in cOut; if prev was found we rebuilt skipping it. */
+    if (false == bCurrentFound) {
+        /* Append current range */
+        if (cOut[0] != '\0') {
+            if (strlen(cOut) + 1 + strlen(cCurrntRange) + 1 < sizeof(cOut)) {
+                strncat(cOut, ",", sizeof(cOut) - strlen(cOut) - 1);
+                strncat(cOut, cCurrntRange, sizeof(cOut) - strlen(cOut) - 1);
+            } else {
+                CcspTraceError(("%s:%d, Resulting reserved ports string too long\n", __FUNCTION__, __LINE__));
+                return;
+            }
+        } else {
+            if (strlen(cCurrntRange) + 1 < sizeof(cOut)) {
+                strncpy(cOut, cCurrntRange, sizeof(cOut) - 1);
+            } else {
+                CcspTraceError(("%s:%d, Resulting reserved ports string too long\n", __FUNCTION__, __LINE__));
+                return;
+            }
+        }
+    } else {
+        CcspTraceInfo(("%s:%d, Current range %s already present; not appending\n", __FUNCTION__, __LINE__, cCurrntRange));
+    }
+
+    CcspTraceInfo(("%s:%d, Local reserved ports updated: %s\n", __FUNCTION__, __LINE__, cOut));
+
+    if (0 != writeSysctl(cOut)) {
         CcspTraceError(("%s:%d, Failed to write %s\n", __FUNCTION__, __LINE__, SYSCTL_PATH));
     }
 }
@@ -206,14 +311,14 @@ void initializeSpeedBoostStructVal()
 
         if (atoi(endPortV4) - atoi(startPortV4) <= MAX_PORT_RANGE_DIFF)
         {
-            appendToLocalReservedPorts(startPortV4, endPortV4);
+            appendToLocalReservedPorts(NULL, startPortV4, endPortV4);
         }
         else
         {
             CcspTraceInfo(("%s:%d, Port range is more than 2000 ports \n", __FUNCTION__, __LINE__));
             char cEndPort[16] = {0};
-            snprintf(cEndPort, sizeof(cEndPort), "%d", atoi(startPortV4) + (MAX_PORT_RANGE_DIFF - 1));
-            appendToLocalReservedPorts(startPortV4, cEndPort);
+            snprintf(cEndPort, sizeof(cEndPort), "%d", atoi(startPortV4) + MAX_PORT_RANGE_DIFF);
+            appendToLocalReservedPorts(NULL, startPortV4, cEndPort);
         }
     }
     
@@ -383,14 +488,14 @@ int processSpeedBoostValue(const char* str, const char* prev_val, char* target_v
 		
             if (atoi(endPortStr_ipv4) - atoi(startPortStr_ipv4) <= MAX_PORT_RANGE_DIFF)
             {
-                appendToLocalReservedPorts(startPortStr_ipv4, endPortStr_ipv4);
+                appendToLocalReservedPorts(prev_val, startPortStr_ipv4, endPortStr_ipv4);
             }
             else
             {
                 CcspTraceInfo(("%s:%d, Port range is more than 2000 ports \n", __FUNCTION__, __LINE__));
                 char cEndPort[16] = {0};
-                snprintf(cEndPort, sizeof(cEndPort), "%d", atoi(startPortStr_ipv4) + (MAX_PORT_RANGE_DIFF - 1));
-                appendToLocalReservedPorts(startPortStr_ipv4, cEndPort);
+                snprintf(cEndPort, sizeof(cEndPort), "%d", atoi(startPortStr_ipv4) + MAX_PORT_RANGE_DIFF);
+                appendToLocalReservedPorts(prev_val, startPortStr_ipv4, cEndPort);
             }
                 CcspTraceInfo(("subtoken is IPv6\n"));
 	        
