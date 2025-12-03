9e693a6687434d9dbd1f27aa7805d0e1a6c6b158
Senthil <senthil.a.kumar@capgemini.com>
2025-12-03 14:34:12 +0530

    RDKB-62594 : Addition of RFC parameter for Stage
    Signed-off-by: Senthil <senthil.a.kumar@capgemini.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.h
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.h
@@ -934,6 +934,56 @@ Identity_SetParamStringValue
 
  APIs for Object:
 
+    Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.STAGE.Enable
+
+    * STAGE_GetParamBoolValue
+    * STAGE_SetParamBoolValue
+***********************************************************************/
+/**
+ * @brief Get boolean parameter value from STAGE object.
+ *
+ * @param[in] hInsContext - The instance handle to the STAGE object.
+ * @param[in] ParamName   - Pointer to the parameter name.
+ * @param[out] pBool      - Pointer to store the boolean value.
+ *
+ * @return The status of the operation.
+ * @retval TRUE if the parameter is found and retrieved successfully.
+ * @retval FALSE if the parameter is not found or operation fails.
+ *
+ */
+
+BOOL
+STAGE_GetParamBoolValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        BOOL*                       pBool
+    );
+/**
+ * @brief Set boolean parameter value for STAGE object.
+ *
+ * @param[in] hInsContext - The instance handle to the STAGE object.
+ * @param[in] ParamName   - Pointer to the parameter name.
+ * @param[in] bValue      - The boolean value to set.
+ *
+ * @return The status of the operation.
+ * @retval TRUE if the parameter is set successfully.
+ * @retval FALSE if the parameter is not found or operation fails.
+ *
+ */
+
+BOOL
+STAGE_SetParamBoolValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        BOOL                        bValue
+    );
+
+/***********************************************************************
+
+ APIs for Object:
+
     DeviceInfo.X_RDKCENTRAL-COM_RFC.Control.DisableNativeHostapd
 
     *  Control_GetParamBoolValue
