15803a67fc0d810287ad4b06f9254cb5d3eb2ad5
aj970 <akshaya_j@comcast.com>
2026-02-10 06:11:06 +0000

    Move Selfheal scripts to cron
    Signed-off-by: aj970 <akshaya_j@comcast.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.h
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.h
@@ -1175,6 +1175,57 @@ ManageableNotification_SetParamBoolValue
 
  APIs for Object:
 
+    Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.Cron.SelfHeal.Enable
+
+    *  SelfHeal_GetParamBoolValue
+    *  SelfHeal_SetParamBoolValue
+
+***********************************************************************/
+
+/**
+ * @brief Get boolean parameter value from SelfHeal object.
+ *
+ * @param[in] hInsContext - The instance handle to the SelfHeal object.
+ * @param[in] ParamName   - Pointer to the parameter name.
+ * @param[out] pBool      - Pointer to store the boolean value.
+ *
+ * @return The status of the operation.
+ * @retval TRUE if the parameter is found and retrieved successfully.
+ * @retval FALSE if the parameter is not found or operation fails.
+ *
+ */
+BOOL
+SelfHeal_GetParamBoolValue
+(
+     ANSC_HANDLE                 hInsContext,
+     char*                       ParamName,
+     BOOL*                       pBool
+);
+
+/**
+ * @brief Set boolean parameter value for SelfHeal object.
+ *
+ * @param[in] hInsContext - The instance handle to the SelfHeal object.
+ * @param[in] ParamName   - Pointer to the parameter name.
+ * @param[in] bValue      - The boolean value to set.
+ *
+ * @return The status of the operation.
+ * @retval TRUE if the parameter is set successfully.
+ * @retval FALSE if the parameter is not found or operation fails.
+ *
+ */
+BOOL
+SelfHeal_SetParamBoolValue
+(
+    ANSC_HANDLE                 hInsContext,
+    char*                       ParamName,
+    BOOL                        bValue
+);
+
+/***********************************************************************
+
+ APIs for Object:
+
     DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WebUI.Enable
 
     *  WebUI_GetParamUlongValue
