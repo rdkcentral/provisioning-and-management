5fd6fc7f5cf0bed8e5a7608472935fa9e1b51231
Gomathi Shankar <gomathishankar37@gmail.com>
2026-03-02 11:41:59 +0530

    RDKB-63473, RDKB-63485: Add RFC Params with handlers for xmeminsight (Enable, Trigger & Args) (#212)
    Reason for change: Add RFC Params with handlers for meminsight (Enable,
    Trigger & Args)
    Test Procedure: Build and verify
    Risks: Medium
    Priority: P1
    
    ---------
    
    Co-authored-by: Goutam Damodaran <140494857+GoutamD2905@users.noreply.github.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.h
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.h
@@ -2640,26 +2640,18 @@ RDKDownloadManager_SetParamStringValue
 
  APIs for Object:
 
-    Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.xMemInsight.Enable
+    Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.meminsight.Enable
+    Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.meminsight.Trigger
+    Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.meminsight.Args
 
     *  xMemInsight_SetParamBoolValue
     *  xMemInsight_GetParamBoolValue
-***********************************************************************/
-
-BOOL xMemInsight_SetParamBoolValue(ANSC_HANDLE hInsContext, char* ParamName, BOOL bValue);
-BOOL xMemInsight_GetParamBoolValue(ANSC_HANDLE hInsContext, char* ParamName, BOOL* pBool);
-
-
-/***********************************************************************
-
- APIs for Object:
-
-    Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.xMemInsight.Args
-
     *  xMemInsight_SetParamStringValue
     *  xMemInsight_GetParamStringValue
 ***********************************************************************/
 
+BOOL xMemInsight_SetParamBoolValue(ANSC_HANDLE hInsContext, char* ParamName, BOOL bValue);
+BOOL xMemInsight_GetParamBoolValue(ANSC_HANDLE hInsContext, char* ParamName, BOOL* pBool);
 ULONG xMemInsight_GetParamStringValue(ANSC_HANDLE hInsContext, char* ParamName, char* pValue, ULONG* pUlSize);
 BOOL xMemInsight_SetParamStringValue(ANSC_HANDLE hInsContext, char* ParamName, char* pString);
 
