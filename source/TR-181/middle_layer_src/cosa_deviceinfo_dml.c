595fee808946a19873c273f162ce3009b9b275de
ksures101 <kavyachowdahalli_suresh@comcast.com>
2026-03-13 04:27:53 +0530

    RDKB-63815, RDKB-63414 : Move RdkLogger Scripts to Cron (#238)
    Reason for change: Running the rdklogger scripts as cron jobs instead of
    background processes when the cron is enabled and running as normal
    background process when the cron is disabled.
    Test Procedure: Check duplicate/many instance of process should not be
    running at any moment in background process/cron mode.
    Priority: P1
    Risks: None
    
    Signed-off-by: KavyaChowdahalli_Suresh@comcast.com
    
    Signed-off-by: KavyaChowdahalli_Suresh@comcast.com
    Co-authored-by: Santosh Nayak <70348540+snayak002c@users.noreply.github.com>

--- source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
+++ source/TR-181/middle_layer_src/cosa_deviceinfo_dml.c
@@ -14354,6 +14354,7 @@ RdkLogger_SetParamBoolValue
             CcspTraceError(("syscfg_set RdkbLogCronEnable failed\n"));
             return FALSE;
         }
+        CcspTraceInfo(("RDKLOGGER RFC updated. Reboot required to apply mode change\n"));
         return TRUE;
     }
     return FALSE;
@@ -18317,7 +18318,7 @@ Logging_SetParamUlongValue
 
 		syscfg_get( NULL, "RdkbLogCronEnable", buf, sizeof(buf));
 
-		if( strcmp(buf, "false") == 0 )
+		if (strcmp(buf, "false") == 0 || strcmp(buf, "") == 0)
 		{
 			CcspTraceInfo(("RdkbLogCronEnable is disabled, running as background process\n"));
 			return TRUE;
