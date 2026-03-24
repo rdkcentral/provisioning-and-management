34aecb8ae2069ffb7769ff7a5846e1acd409d334
SanthoshGujulvajagadeesh <68006718+SanthoshGujulvajagadeesh@users.noreply.github.com>
2026-03-24 14:32:38 +0000

    Revert "RDKB-63389: dibbler-server service fails to start after switching network mode from Bridge-Static to Router (#185)"
    This reverts commit 72c40ad07b8a269587699cbbc210096809813228.

--- source-arm/TR-181/board_sbapi/cosa_dhcpv4_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_dhcpv4_apis.c
@@ -2460,7 +2460,6 @@ ANSC_STATUS CosaDmlDhcpsSetIpv4Status()
         int            se_fd = -1;
         char           evtValue[10]={'\0'};
 
-        // Don't close the fd returned by s_sysevent_connect as it is a shared fd
         se_fd = s_sysevent_connect(&se_token);
         if (0 > se_fd) {
 
@@ -2489,7 +2488,9 @@ ANSC_STATUS CosaDmlDhcpsSetIpv4Status()
                 }
             }
         }
+  	sysevent_close(se_fd,se_token);
         return ANSC_STATUS_SUCCESS;
+
 }
 
 ANSC_STATUS
@@ -2595,7 +2596,6 @@ CosaDmlDhcpsGetPoolInfo
         char           dhcp_status[64];
         int            se_fd = -1;
 
-        // Don't close the fd returned by s_sysevent_connect as it is a shared fd
         se_fd = s_sysevent_connect(&se_token);
         if (0 > se_fd) {
 
@@ -2606,9 +2606,11 @@ CosaDmlDhcpsGetPoolInfo
         }
         else
         {
-            /* Get DHCP Server Status */
+            /* Get DHCP Server Status */             
             sysevent_get(se_fd, se_token, "dhcp_server-status", dhcp_status, sizeof(dhcp_status));
+            sysevent_close(se_fd,se_token);
             AnscTraceFlow(("%s: dhcp_status = %s\n", __FUNCTION__, dhcp_status));
+             
         }
 
         if (0 == strcmp(dhcp_status, "started")) {
