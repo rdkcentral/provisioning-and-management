cab0bf1374d0bbad3ae8bdce4625e2bc80ec0c4d
KIRANYA R <98633437+kiranya25@users.noreply.github.com>
2026-04-08 15:01:20 +0530

    Add files via upload

---
+++ source/TR-181/middle_layer_src/pam_register_component.h
@@ -0,0 +1,44 @@
+#ifndef _PAM_REGISTER_COMPONENT_H_
+#define _PAM_REGISTER_COMPONENT_H_
+
+#include <stdbool.h>
+#include <rbus/rbus.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Check WiFi dependencies and publish wifi_ready_to_go
+ *
+ * This API checks the hardcoded WiFi dependencies:
+ *  - com.cisco.spvtg.ccsp.psm
+ *  - com.cisco.spvtg.ccsp.pam
+ *
+ * If all dependencies are up (discovered via RBUS),
+ * a wifi_ready_to_go event is published using RBUS.
+ *
+ * @param handle  RBUS handle obtained by PAM
+ */
+void pam_checkAndPublishWifiReady(rbusHandle_t handle);
+
+/**
+ * @brief Check WAN Manager dependencies and publish wan_ready_to_go
+ *
+ * This API checks the hardcoded WAN dependencies:
+ *  - com.cisco.spvtg.ccsp.psm
+ *  - com.cisco.spvtg.ccsp.pam
+ *  - com.cisco.spvtg.ccsp.ledmanager
+ *
+ * If all dependencies are up (discovered via RBUS),
+ * a wan_ready_to_go event is published using RBUS.
+ *
+ * @param handle RBUS handle obtained by PAM
+ */
+void pam_checkAndPublishWanReady(rbusHandle_t handle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PAM_REGISTER_COMPONENT_H_ */
