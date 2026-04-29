0f79cbf20f22de0e92b4fcc6695f6117b4ad57f8
Leela Charumathi M <LeelaCharumathi_M@comcast.com>
2026-04-29 09:07:27 +0000

    XF10-715 : WebUI.Enable = MSOonly not opening MSO GUI
    Reason for change: WANIPV6 is empty for XF10
    Test Procedure: Able to access MSO
    Priority: P1
    Risks: Low
    
    Signed-off-by: Leela Charumathi M <LeelaCharumathi_M@comcast.com>

--- custom/comcast/source/TR-181/custom_sbapi/cosa_deviceinfo_apis_custom.c
+++ custom/comcast/source/TR-181/custom_sbapi/cosa_deviceinfo_apis_custom.c
@@ -325,19 +325,12 @@ CosaDmlDiGetRouterIPv6Address
     {
         CosaUtilGetIpv6AddrInfo("brlan0", &p_v6addr, &v6addr_num);
     }
-#if defined(_SCXF11BFL_PRODUCT_REQ_)
     else
     {
         char wan_interface[32] = {0};
         commonSyseventGet("current_wan_ifname", wan_interface, sizeof(wan_interface));
         CosaUtilGetIpv6AddrInfo(wan_interface, &p_v6addr, &v6addr_num);
     }
-#else
-    else
-    {
-        CosaUtilGetIpv6AddrInfo("erouter0", &p_v6addr, &v6addr_num);
-    }
-#endif
 #elif defined(_HUB4_PRODUCT_REQ_)
 	CosaUtilGetIpv6AddrInfo("brlan0", &p_v6addr, &v6addr_num);
 #elif defined(_WNXL11BWL_PRODUCT_REQ_)
