d326c0f1797edfc78e72d4dc9aa9154860cd15fc
Netaji Panigrahi <114923459+NetajiPanigrahi@users.noreply.github.com>
2026-05-15 13:02:26 +0530

    Update cosa_rbus_handler_apis.c

--- source/TR-181/middle_layer_src/cosa_rbus_handler_apis.c
+++ source/TR-181/middle_layer_src/cosa_rbus_handler_apis.c
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// Added for test
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
