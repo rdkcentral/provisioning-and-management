3481baa34494be01ad9fcebeeaca988fb5ca547a
skrist381 <sudharshanreddy_kristipati@comcast.com>
2026-05-29 09:54:43 +0530

    RDKB-62908 [PAM] [5GB]Reduce Repetitive Logging (#281)
    Reason for change: Removed the repetitive logs from PAM
    Test Procedure: not applicable
    Risks: Low
    Priority: P1
    
    Signed-off-by: Sudharshan Reddy <sudharshanreddy_kristipati@comcast.com>
    Co-authored-by: apattu200 <alagupandi_pattu@comcast.com>

--- source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_deviceinfo_apis.c
@@ -1786,8 +1786,8 @@ void COSADmlGetProcessInfo(PCOSA_DATAMODEL_PROCSTATUS p_info)
 
     p_info->ProcessNumberOfEntries = i;
 
-    fprintf(stderr,"\n %s %d  ProcessNumberOfEntries:%lu",__func__,__LINE__,p_info->ProcessNumberOfEntries);
-    CcspTraceWarning(("\n %s %d  ProcessNumberOfEntries:%lu\n",__func__,__LINE__,p_info->ProcessNumberOfEntries));
+    fprintf(stderr," %s %d  ProcessNumberOfEntries:%lu\n",__func__,__LINE__,p_info->ProcessNumberOfEntries);
+    CcspTraceWarning((" %s %d  ProcessNumberOfEntries:%lu\n",__func__,__LINE__,p_info->ProcessNumberOfEntries));
 }
 
 void test_get_proc_info()
@@ -1958,8 +1958,8 @@ ULONG COSADmlGetProcessNumberOfEntries()
         dir = NULL;
     }
 
-    fprintf(stderr,"\n %s %d  ProcessNumberOfEntries:%lu", __func__, __LINE__, i);
-    CcspTraceWarning(("\n %s %d  ProcessNumberOfEntries:%lu\n", __func__, __LINE__, i));
+    fprintf(stderr," %s %d  ProcessNumberOfEntries:%lu\n", __func__, __LINE__, i);
+    CcspTraceWarning((" %s %d  ProcessNumberOfEntries:%lu\n", __func__, __LINE__, i));
     return i;
 }
 
