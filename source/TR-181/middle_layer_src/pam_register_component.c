e8b781cc01d4c7801b3b15acdabdf4f7d8bb6dac
Kiranya R <Kiranya_R@comcast.com>
2026-06-05 05:58:52 +0000

    PAM:updat in EventHandlers
    Change-Id: I399947e2a2c0731963873ccefd7f933318bcbfd6

--- source/TR-181/middle_layer_src/pam_register_component.c
+++ source/TR-181/middle_layer_src/pam_register_component.c
@@ -403,6 +403,7 @@ static rbusError_t systemReadyGetHandler(
     rbusGetHandlerOptions_t* opts)
 {
     (void)handle;
+    (void)opts;
 
     const char* name = rbusProperty_GetName(property);
 
@@ -415,9 +416,6 @@ static rbusError_t systemReadyGetHandler(
 
         rbusProperty_SetValue(property, value);
         rbusValue_Release(value);
-
-        const char* caller = (opts && opts->requestingComponent) ? opts->requestingComponent : "unknown";
-        CcspTraceInfo(("[PAM] GET Device.CR.SystemReady = %d caller=%s\n", isSystemReady, caller));
 
         return RBUS_ERROR_SUCCESS;
     }
