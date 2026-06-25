5126376ca801a0d7b4f7aedd744f6cb8d2f93d53
satish-kotapati <skotapati@maxlinear.com>
2026-06-25 21:01:51 +0530

    RDKCOM-5422: RDKBDEV-3258: Remove UTOPIA mcast services from the build (#62)
    Reason for change: adding flag to control IGMPProxyEnable set parameter
    Dependant PR:https://github.com/rdkcentral/utopia/pull/80
    Risks: None
    
    Signed-off-by: sai satish kotapati  <skotapati@maxlinear.com>
    Co-authored-by: Goutam Damodaran <140494857+GoutamD2905@users.noreply.github.com>
    Co-authored-by: Santosh Nayak <70348540+snayak002c@users.noreply.github.com>

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -3170,6 +3170,7 @@ CosaDmlDcGetIGMPProxyEnable
     return ANSC_STATUS_SUCCESS;
 }
 
+#ifndef DISABLE_IGMPPROXY
 ANSC_STATUS
 CosaDmlDcSetIGMPProxyEnable
     (
@@ -3200,6 +3201,7 @@ CosaDmlDcSetIGMPProxyEnable
     }
     return ANSC_STATUS_SUCCESS;
 }
+#endif
 
 ANSC_STATUS
 CosaDmlDcGetDNSProxyEnable
