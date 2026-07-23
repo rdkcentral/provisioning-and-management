08e4a7029e5a97a9d24025351452fd52b1f5c0fc
anatar818 <151121941+anatar818@users.noreply.github.com>
2026-07-23 21:01:10 +0530

    Update cosa_ip_apis.c

--- source-arm/TR-181/board_sbapi/cosa_ip_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_ip_apis.c
@@ -444,9 +444,9 @@ CosaDmlIpInit
         {
            rc = strcpy_s((char *)g_ipif_names[i],sizeof(g_ipif_names[i]), G_USG_IF_NAME(i));
 #if defined(FEATURE_RDKB_CONFIGURABLE_WAN_INTERFACE)
-          char out_value[20] = {0};
+          char out_value[64] = {0};
           if (!syscfg_get(NULL, "wan_physical_ifname", out_value, sizeof(out_value)))
-              strcpy_s((char *)g_ipif_names[i], sizeof(g_ipif_names[i]), out_value);
+              rc = strcpy_s((char *)g_ipif_names[i], sizeof(g_ipif_names[i]), out_value);
 #endif
         }
         else
