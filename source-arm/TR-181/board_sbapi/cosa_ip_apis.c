1c1bcc1ad68f6845ebc83744389bec7e6dedfbd0
anatar818 <151121941+anatar818@users.noreply.github.com>
2026-07-27 19:25:42 +0530

    Configurable wan depends on syscfg only

--- source-arm/TR-181/board_sbapi/cosa_ip_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_ip_apis.c
@@ -443,21 +443,10 @@ CosaDmlIpInit
  if (i == 0)
  {
         rc = strcpy_s((char *)g_ipif_names[i],sizeof(g_ipif_names[i]), G_USG_IF_NAME(i));
-#if defined(FEATURE_RDKB_CONFIGURABLE_WAN_INTERFACE)
+#if defined(FEATURE_RDKB_CONFIGURABLE_WAN_INTERFACE) 
         char out_value[64] = {0};
-		FILE *fp = NULL;
-		fp = fopen("/nvram/wan_name.txt", "r");
-        if (fp != NULL)
-        {
-			 fprintf(fp, "%s", out_value);
-			 rc = strcpy_s((char *)g_ipif_names[i], sizeof(g_ipif_names[i]), out_value);
-			 fclose(fp);
-		}
-	    else
-		{
-			 if (!syscfg_get(NULL, "wan_physical_ifname", out_value, sizeof(out_value)))
-				 rc = strcpy_s((char *)g_ipif_names[i], sizeof(g_ipif_names[i]), out_value);  
-		}	       
+		if (!syscfg_get(NULL, "wan_physical_ifname", out_value, sizeof(out_value)))
+			rc = strcpy_s((char *)g_ipif_names[i], sizeof(g_ipif_names[i]), out_value);         
 #endif
  }
  else
