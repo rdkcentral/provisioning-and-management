ff7d326c55f0ec07ace5e35ca0a0b3a00fbe5bd5
anatar818 <151121941+anatar818@users.noreply.github.com>
2026-07-27 13:41:27 +0530

    RDKBDEV-3470,RDKBACCL-1945 : Device Not coming Online for Configurable WAN Ethernet Based Code flow
    Reason for change : In Ethernet based code flow , the Device.IP.Interface DM's are not updating with the configurred wan name when Configurable wan is enabled
    Test Procedure : Customise the wan name and check the DM's , it is affecting the components like Tr069
    Risks : Low
    Priority : P1

--- source-arm/TR-181/board_sbapi/cosa_ip_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_ip_apis.c
@@ -440,6 +440,27 @@ CosaDmlIpInit
 	   g_usg_if_cfg[i].LinkType = ( TRUE == IsThisCurrentPartnerID("sky-") ) ? COSA_DML_LINK_TYPE_VlanLink : COSA_DML_LINK_TYPE_EthLink;   
 	}
 #endif
+ if (i == 0)
+ {
+        rc = strcpy_s((char *)g_ipif_names[i],sizeof(g_ipif_names[i]), G_USG_IF_NAME(i));
+#if defined(FEATURE_RDKB_CONFIGURABLE_WAN_INTERFACE)
+        char out_value[64] = {0};
+		FILE *fp = NULL;
+		fp = fopen("/nvram/wan_name.txt", "r");
+        if (fp != NULL)
+        {
+			 fprintf(fp, "%s", out_value);
+			 rc = strcpy_s((char *)g_ipif_names[i], sizeof(g_ipif_names[i]), out_value);
+			 fclose(fp);
+		}
+	    else
+		{
+			 if (!syscfg_get(NULL, "wan_physical_ifname", out_value, sizeof(out_value)))
+				 rc = strcpy_s((char *)g_ipif_names[i], sizeof(g_ipif_names[i]), out_value);  
+		}	       
+#endif
+ }
+ else
         rc = strcpy_s((char *)g_ipif_names[i],sizeof(g_ipif_names[i]), G_USG_IF_NAME(i));
         ERR_CHK(rc);
         if ((gDmsbIpIfLoopbackInstNum == 0) && (strcmp(G_USG_IF_NAME(i), "lo") == 0))
