e42c725c5007e9cb07a745eda93c259ee634ddd2
Vysakh A V <vysakhav@protonmail.com>
2026-09-14 12:04:06 +0100

    Fixup - Correct commands to properly delete contents on FR

--- source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_x_cisco_com_devicecontrol_apis.c
@@ -2085,31 +2085,8 @@ void* restoreAllDBs(void* arg)
 
 #if defined(_SCXF11BFL_PRODUCT_REQ_)
         /* Remove /data & /nvram - Preserve scratchpad, core data and required decryption keys. */
-	v_secure_system(
-             "find /nvram /nvram2 /data -mindepth 1 ! \\( "
-             "-path '/nvram/.partner_ID' -o "
-             "-regex '.*/Q[[:xdigit:]]\\{8\\}$' -o "
-             "-path '/nvram/.apply_partner_defaults' -o "
-             "-path '/data/scratchpad' -o "
-             "-path '/data/core.new' -o "
-             "-path '/data/core.last' "
-             "\\) -exec rm -rf -- {} +; "
-             "sync"
-);
-        /* Remove wifi configuration. */
-	v_secure_system(
-             "rm -rf "
-             "/data/.comcast_config_set.done "
-             "/data/nvram_cfg.txt "
-             "/data/psi* "
-             "/data/.nvram_restore_cfg.txt "
-             "/data/.user_nvram.setting "
-             "/data/.kernel_nvram.setting "
-             "/data/onewifi_downgrade_required "
-             "/nvram/.bcmwifi_xhs_lnf_enabled "
-             "/nvram/secure/wifi/* "
-             "/nvram/wifi/*"
-        );
+        v_secure_system("sync; find /nvram /nvram2 /data -mindepth 1 | grep -vE \"^/nvram/\\.partner_ID$|^/nvram/\\.apply_partner_defaults$|^/data/scratchpad$|^/data/core\\.new$|^/data/core\\.last$|.*/Q[[:xdigit:]]{8}$\" | xargs rm -rf; sync");
+        v_secure_system("sync; rm -rf /data/.comcast_config_set.done /data/.nvram_restore_cfg.txt /data/.user_nvram.setting /data/.kernel_nvram.setting /nvram/.bcmwifi_xhs_lnf_enabled /nvram/secure/wifi/* /nvram/wifi/*; sync");
         v_secure_system("sync; touch /data/.do_fr_on_boot; sync");
         v_secure_system("mkdir -p /nvram/secure/data/ && touch /nvram/secure/data/syscfg.db");
         v_secure_system("echo \"X_RDKCENTRAL-COM_LastRebootReason=factory-reset\" > /nvram/secure/data/syscfg.db");
