29e7dd32d38d848dcfdf0f062bf542a7afee574d
ss192 <Selvaranjani_S@comcast.com>
2026-09-18 07:20:50 +0000

    XF10-822 WAN Default Gateway Address is shown as empty in WEBUI

--- source-arm/TR-181/board_sbapi/cosa_routing_apis.c
+++ source-arm/TR-181/board_sbapi/cosa_routing_apis.c
@@ -2916,7 +2916,6 @@ Route6_GetRouteTable(const char *ifname, RouteInfo6_t infos[], int *numInfo)
      * because of "proto" (orig) info,
      * we use "ip -6 route" instead of "route -A inet6".
      */
-    CcspTraceInfo(("Ranjani --> %s %d  \n", __FUNCTION__,__LINE__));
     if ((fp = v_secure_popen("r", "ip -6 route show dev %s", ifname)) == NULL)
         return -1;
 
@@ -2934,13 +2933,8 @@ Route6_GetRouteTable(const char *ifname, RouteInfo6_t infos[], int *numInfo)
         bzero(info6, sizeof(RouteInfo6_t));
 
         if (strcmp(prefix, "default") == 0)
-        {
-            #if defined(_SCXF11BFL_PRODUCT_REQ_)
-                snprintf(info6->prefix, sizeof(info6->prefix), "::/0");
-            #else
-                continue;
-            #endif
-        }
+        //    snprintf(info6->prefix, sizeof(info6->prefix), "::/0");
+            continue;
         else
             snprintf(info6->prefix, sizeof(info6->prefix), "%s", prefix);
 
@@ -3000,17 +2994,7 @@ Route6_GetRouteTable(const char *ifname, RouteInfo6_t infos[], int *numInfo)
     }
         v_secure_pclose(fp);
 	//Fix for issue RDKB-367
-#if defined(_COSA_BCM_MIPS_) || defined(_ENABLE_DSL_SUPPORT_)
-     CcspTraceInfo(("Ranjani main table%s %d \n", __FUNCTION__,__LINE__));
     snprintf(cmd, sizeof(cmd), "ip -6 route list table main");
-#else 
-     CcspTraceInfo(("Ranjani erouter table%s %d \n", __FUNCTION__,__LINE__));
-    snprintf(cmd, sizeof(cmd), "ip -6 route list table erouter");
-#endif
-#if  defined (_SCXF11BFL_PRODUCT_REQ_)
- CcspTraceInfo(("Ranjani XF11 main %s %d \n", __FUNCTION__,__LINE__));
-    snprintf(cmd, sizeof(cmd), "ip -6 route list table main");
-#endif    
     if ((fp = popen(cmd, "r")) == NULL)
         return -1;
 	
